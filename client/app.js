d8bb92ecd54356b0615cb0445930b1fcd1230d7b
David Lau <David.Lau@TRADELINKTECHNOLOGIES.TLT>
2020-06-18 18:40:34 +0800

    add GitHub

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 const App = () => {
-  return <div>Hello from React! new time</div>
+  return <div>Hello from React! I have connect GitHub!</div>
 }
 
 export default App;
