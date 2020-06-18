49f4cb90fa579374d1066510b7c3fb7d5bc4641c
David Lau <David.Lau@TRADELINKTECHNOLOGIES.TLT>
2020-06-18 10:49:24 +0800

    from pc

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const Bundler = require('parcel-bundler');
 const path = require('path');
-const api = require('./server/api');
-
-const file = path.join(__dirname, 'client/index.html');
-const options = {
-  outDir: './client/dist',
-  cacheDir: './client/.cache',
-};
-const bundler = new Bundler(file, options);
+const api = require('./api');
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 app.use('/api', api);
 if (process.env.NODE_ENV !== 'production') {
-  app.use(express.static(path.join(__dirname, 'client/dist')));
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
-  });
-} else {
+  const file = path.join(__dirname, '../client/index.html');
+  const options = {
+    outDir: './client/dist',
+    cacheDir: './client/.cache',
+  };
+  const bundler = new Bundler(file, options);
   app.use(bundler.middleware());
+} else {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Password generator listening on ${port}`);
 });
