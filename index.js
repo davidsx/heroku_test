6d150e4ca7db2778856c630c771564a69898130a
David Lau <David.Lau@TRADELINKTECHNOLOGIES.TLT>
2020-06-18 10:40:31 +0800

    from pc

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(express.static(path.join(__dirname, 'client/dist')));
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/dist/index.html'));
-  })
-  
+  });
 } else {
   app.use(bundler.middleware());
 }
